6dc743130872b43ce58b95087104ffcbf43e8469
vic9nt-goat <dvicente.infinitybot@gmail.com>
2026-07-25 12:19:55 -0600

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const Reminder = require('./models/Reminder');
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMembers, // Vital para capturar el ingreso de usuarios
+    GatewayIntentBits.GuildMembers,
     GatewayIntentBits.GuildMessages,
     GatewayIntentBits.MessageContent
   ]
@@ -41,7 +41,6 @@ app.get('/', async (req, res) => {
   }
 });
 
-// Cargador automático de eventos (lee la carpeta events para detectar guildMemberAdd.js)
 const eventsPath = path.join(__dirname, 'events');
 if (fs.existsSync(eventsPath)) {
   const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
